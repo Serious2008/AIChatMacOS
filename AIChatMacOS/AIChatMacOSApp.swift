6c96100d242b8abcda043987d71c354522adfabc
Sergey Markov <msergei2008@gmail.com>
2025-08-10 21:35:05 +0500

    Initial Commit

---
+++ AIChatMacOS/AIChatMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatMacOSApp.swift
+//  AIChatMacOS
+//
+//  Created by Sergey Markov on 10.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
